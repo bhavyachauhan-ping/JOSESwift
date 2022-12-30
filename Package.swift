a9efe42e83ad65d62a3bf72c9443e39d30121add
bhavyachauhan <bhavyachauhan@pingidentity.com>
2022-12-30 12:30:53 -0600

    secp256k1 package dependency

--- Package.swift
+++ Package.swift
@@ -9,6 +9,8 @@ let package = Package(
     ],
     dependencies: [.package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", from: "0.10.0")],
     targets: [
-        .target(name: "JOSESwift", path: "JOSESwift")
+        .target(name: "JOSESwift",
+                dependencies: [.product(name: "secp256k1", package: "secp256k1.swift")],
+                path: "JOSESwift")
     ],
     swiftLanguageVersions: [.v5])
