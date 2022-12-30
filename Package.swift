38d58c397b1cb1e1f440727fa2077639891a1389
bhavyachauhan <bhavyachauhan@pingidentity.com>
2022-12-30 15:12:16 -0600

    Update

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "JOSESwift",
-    platforms: [.iOS(.v10), .macOS(.v10_15), .watchOS(.v4)],
+    platforms: [.iOS(.v11)],
     products: [
         .library(name: "JOSESwift", targets: ["JOSESwift"])
     ],
