18e725f18818982825557ae84e1dc68b5afce160
Caleb Cho <calebcho@pingidentity.com>
2023-01-24 13:24:04 -0500

    normalize signature before running verification

--- JOSESwift/Sources/Secp256k1Verifier.swift
+++ JOSESwift/Sources/Secp256k1Verifier.swift
@@ -18,9 +18,17 @@ internal struct Secp256k1Verifier: VerifierProtocol {
 
     func verify(_ verifyingInput: Data, against signature: Data) throws -> Bool {
         let secp256k1PublicKey = try secp256k1.Signing.PublicKey(rawRepresentation: self.publicKey, format: .uncompressed)
-        let ecdsaSignature = try secp256k1.Signing.ECDSASignature(compactRepresentation: signature)
-        return secp256k1PublicKey.ecdsa.isValidSignature(ecdsaSignature, for: verifyingInput)
+        
+        var signatureIn = secp256k1_ecdsa_signature()
+        var signatureOut = secp256k1_ecdsa_signature()
+        secp256k1_ecdsa_signature_parse_compact(secp256k1.Context.raw, &signatureIn, signature)
+        secp256k1_ecdsa_signature_normalize(secp256k1.Context.raw, &signatureOut, &signatureIn)
+        
+        guard let normalizedSignature = try? secp256k1.Signing.ECDSASignature(rawRepresentation: signatureOut.dataValue) else {
+            throw JOSESwiftError.signatureInvalid
+        }
 
+        return secp256k1PublicKey.ecdsa.isValidSignature(normalizedSignature, for: verifyingInput)
     }
 }
 
