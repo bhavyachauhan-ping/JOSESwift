fd804cdf0d7cd1964dc09e9d342e6d4b366970de
bhavyachauhan <bhavyachauhan@pingidentity.com>
2023-03-16 16:01:13 -0500

    Updating dependency

--- Package.swift
+++ Package.swift
@@ -7,10 +7,10 @@ let package = Package(
     products: [
         .library(name: "JOSESwift", targets: ["JOSESwift"])
     ],
-    dependencies: [.package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", from: "0.10.0")],
+    dependencies: [.package(url: "https://github.com/bhavyachauhan-ping/secp256k1.swift", Package.Dependency.Requirement.branch("secp256k1lib"))],
     targets: [
         .target(name: "JOSESwift",
-                dependencies: [.product(name: "secp256k1", package: "secp256k1.swift")],
+                dependencies: [.product(name: "secp256k1lib", package: "secp256k1.swift")],
                 path: "JOSESwift")
     ],
     swiftLanguageVersions: [.v5])
